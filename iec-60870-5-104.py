96d9127adef88239c566cd3e2ba2ef15c2d60120
guoff <guocdfeifei@163.com>
2021-03-12 19:23:12 +0800

    优化

--- iec-60870-5-104.py
+++ iec-60870-5-104.py
@@ -58,7 +58,7 @@ def iec104(dst):
     except:
         return '', -1
     # =========================================================================
-    #创建链接
+    #创建链接  主站连接子站时，主站给子站发送一个U帧启动报文
     TESTFR = [
         # iec 104 apci layer
         0x68,  # start
@@ -90,18 +90,19 @@ def iec104(dst):
         print "testfr: nothing received"
         return recv, -1
 
-    # =========================================================================
-    #获取数据
-    STARTDT = [
+    # 接收→确认激活传输启动
+    TESTFR = [
         # iec 104 apci layer
         0x68,  # start
         0x04,  # APDU len
-        0x43,  # type 0000 0111
+        0x0B,  # type 0100 0011
         0x00, 0x00, 0x00  # padding
+
     ]
 
-    sock.send(''.join(map(chr, STARTDT)))
+    sock.send(''.join(map(chr, TESTFR)))
     recv = recv_from_socket(sock)
+    # print "0testfr recv: %s" % recv
     while not recv:
         recv = recv_from_socket(sock)
         time.sleep(0.5)
@@ -114,14 +115,38 @@ def iec104(dst):
         print 'startdt: nothing received'
         return recv, -1
 
-    # if received 2 packets - STARTDT con + ME_EI_NA_1 Init  - full length should be 6+6+10 bytes
-    if len(recv) == 22:
-        asdu_addr, = struct.unpack('<H', recv[16:18])
-        print "ASDU address: %d" % asdu_addr
-        sock.close()
-        return recv, asdu_addr
     # =========================================================================
-
+    # #获取数据
+    # STARTDT = [
+    #     # iec 104 apci layer
+    #     0x68,  # start
+    #     0x04,  # APDU len
+    #     0x43,  # type 0000 0111
+    #     0x00, 0x00, 0x00  # padding
+    # ]
+    #
+    # sock.send(''.join(map(chr, STARTDT)))
+    # recv = recv_from_socket(sock)
+    # while not recv:
+    #     recv = recv_from_socket(sock)
+    #     time.sleep(0.5)
+    # if recv:
+    #     logging.info('{0}'.format(dst))
+    #     logging.debug('iec104 STARTDT : recv: %s' % recv.encode('hex'))
+    #     # print "recv: %r" % recv
+    #     print "startdt recv: %s" % recv.encode('hex')
+    # else:
+    #     print 'startdt: nothing received'
+    #     return recv, -1
+    #
+    # # if received 2 packets - STARTDT con + ME_EI_NA_1 Init  - full length should be 6+6+10 bytes
+    # if len(recv) == 22:
+    #     asdu_addr, = struct.unpack('<H', recv[16:18])
+    #     print "ASDU address: %d" % asdu_addr
+    #     sock.close()
+    #     return recv, asdu_addr
+    # =========================================================================
+    #发起总召唤
     C_IC_NA_1_broadcast = [
 
         # iec 104 apci layer
@@ -135,9 +160,9 @@ def iec104(dst):
         0x01,  # numix
         0x06,  # some stuff
         0x00,  # OA
-        0xff, 0xff,  # addr 65535
+        0x01, 0x00,  # addr 65535
         0x00,  # IOA
-        0x00, 0x00, 0x00  # 0x14
+        0x00, 0x00, 0x14  # 0x14
 
     ]
 
@@ -154,6 +179,202 @@ def iec104(dst):
     else:
         print 'c_ic_na_1_broadcast: nothing received'
         return recv, -1
+
+    # 接收→S帧
+    print("接收→S帧")
+    STARTDT = [
+        # iec 104 apci layer
+        0x68,  # start
+        0x04,  # APDU len
+        0x01,  # type 0000 0111
+        0x00, 0x02, 0x00  # padding
+    ]
+    sock.send(''.join(map(chr, STARTDT)))
+    recv = recv_from_socket(sock)
+    while not recv:
+        recv = recv_from_socket(sock)
+        time.sleep(0.5)
+    if recv:
+        logging.info('{0}'.format(dst))
+        logging.debug('iec104 C_IC_NA_1_broadcast : recv: %s' % recv.encode('hex'))
+        # print "recv: %r" % recv
+        print "c_ic_na_1 recv: %s" % recv.encode('hex')
+    else:
+        print 'c_ic_na_1_broadcast: nothing received'
+        return recv, -1
+
+    #接收→总召唤确认 680401000200
+    if recv.encode('hex') == '680401000200':
+        #发送确认命令
+        print("发送确认命令")
+        # 发起总召唤确认命令
+        C_IC_NA_1_broadcast = [
+
+            # iec 104 apci layer
+            0x68,  # start
+            0x0e,  # apdu len
+            0x00, 0x00,  # type + tx
+            0x00, 0x00,  # rx
+
+            # iec 104 asdu layer
+            0x64,  # type id: C_IC_NA_1, interrogation command
+            0x01,  # numix
+            0x06,  # some stuff
+            0x00,  # OA
+            0x01, 0x00,  # addr 65535
+            0x00,  # IOA
+            0x00, 0x00, 0x14  # 0x14
+
+        ]
+
+        sock.send(''.join(map(chr, C_IC_NA_1_broadcast)))
+        recv = recv_from_socket(sock)
+        while not recv:
+            recv = recv_from_socket(sock)
+            time.sleep(0.5)
+        if recv:
+            logging.info('{0}'.format(dst))
+            logging.debug('iec104 STARTDT : recv: %s' % recv.encode('hex'))
+            # print "recv: %r" % recv
+            print "startdt recv: %s" % recv.encode('hex')
+        else:
+            print 'startdt: nothing received'
+            return recv, -1
+
+        #发送S帧
+        print("发送S帧")
+        STARTDT = [
+            # iec 104 apci layer
+            0x68,  # start
+            0x04,  # APDU len
+            0x01,  # type 0000 0111
+            0x00, 0x02, 0x00  # padding
+        ]
+        sock.send(''.join(map(chr, STARTDT)))
+        #接收→总召唤确认
+        print("接收→总召唤确认")
+        C_IC_NA_1_broadcast = [
+
+            # iec 104 apci layer
+            0x68,  # start
+            0x0e,  # apdu len
+            0x00, 0x00,  # type + tx
+            0x00, 0x00,  # rx
+
+            # iec 104 asdu layer
+            0x64,  # type id: C_IC_NA_1, interrogation command
+            0x01,  # numix
+            0x07,  # some stuff
+            0x00,  # OA
+            0x01, 0x00,  # addr 65535
+            0x00,  # IOA
+            0x00, 0x00, 0x14  # 0x14
+
+        ]
+
+        sock.send(''.join(map(chr, C_IC_NA_1_broadcast)))
+
+
+        #发送S帧
+        print("发送S帧")
+        STARTDT = [
+            # iec 104 apci layer
+            0x68,  # start
+            0x04,  # APDU len
+            0x01,  # type 0000 0111
+            0x00, 0x02, 0x00  # padding
+        ]
+        sock.send(''.join(map(chr, STARTDT)))
+
+
+        #接收→YX帧（以类型标识1为例）
+        print("接收→YX帧（以类型标识1为例）")
+        C_IC_NA_1_broadcast = [
+
+            # iec 104 apci layer
+            0x68,  # start
+            0x1e,  # apdu len
+            0x04, 0x00,  # type + tx
+            0x02, 0x00,  # rx
+
+            # iec 104 asdu layer
+            0x03,  # type id: C_IC_NA_1, interrogation command
+            0x05,  # numix
+            0x14,  # some stuff
+            0x01,  # OA
+            0x00, 0x01,  # addr 65535
+            0x00,  # IOA
+            0x00, 0x02, 0x06, 0x00, 0x02 , 0x0A, 0x00, 0x00, 0x01 , 0x0B, 0x00, 0x00, 0x02 , 0x0C, 0x00, 0x00, 0x01 # 0x14
+
+        ]
+
+        sock.send(''.join(map(chr, C_IC_NA_1_broadcast)))
+
+
+        #发送S帧
+        print("发送S帧")
+        STARTDT = [
+            # iec 104 apci layer
+            0x68,  # start
+            0x04,  # APDU len
+            0x01,  # type 0000 0111
+            0x00, 0x02, 0x00  # padding
+        ]
+        sock.send(''.join(map(chr, STARTDT)))
+
+
+
+
+        #接收→结束总召唤帧
+        print("接收→结束总召唤帧")
+        C_IC_NA_1_broadcast = [
+
+            # iec 104 apci layer
+            0x68,  # start
+            0x0e,  # apdu len
+            0x08, 0x00,  # type + tx
+            0x02, 0x00,  # rx
+
+            # iec 104 asdu layer
+            0x64,  # type id: C_IC_NA_1, interrogation command
+            0x01,  # numix
+            0x0A,  # some stuff
+            0x00,  # OA
+            0x01, 0x00,  # addr 65535
+            0x00,  # IOA
+            0x00, 0x00, 0x14 # 0x14
+
+        ]
+
+        sock.send(''.join(map(chr, C_IC_NA_1_broadcast)))
+
+
+        #发送S帧
+        print("发送S帧")
+        STARTDT = [
+            # iec 104 apci layer
+            0x68,  # start
+            0x04,  # APDU len
+            0x01,  # type 0000 0111
+            0x00, 0x02, 0x00  # padding
+        ]
+        sock.send(''.join(map(chr, STARTDT)))
+
+
+        recv = recv_from_socket(sock)
+        while not recv:
+            recv = recv_from_socket(sock)
+            time.sleep(0.5)
+        if recv:
+            logging.info('{0}'.format(dst))
+            logging.debug('iec104 C_IC_NA_1_broadcast : recv: %s' % recv.encode('hex'))
+            # print "recv: %r" % recv
+            print "c_ic_na_1 recv: %s" % recv.encode('hex')
+        else:
+            print 'c_ic_na_1_broadcast: nothing received'
+            return recv, -1
+    else:
+        print(recv.encode('hex'))
 
     # print "recv: %s" % recv.encode('hex')
     try:
@@ -184,23 +405,23 @@ if __name__ == '__main__':
             print "ip: {0}, recv: {1}, asdu_addr: {2}".format(ip, recv.encode('hex'), asdu_addr)
 
     #继电器部分
-    relay = relayop()
-    time.sleep(1)
-    relay.closeall()
-    time.sleep(1)
-    state = 0
-
-
-    def opRelay(Relay):
-        global state
-        if state == 0:
-            relay.open(Relay)
-            state = 1
-        else:
-            relay.close(Relay)
-            state = 0
-
-
-    for i in range(10):
-        opRelay(1)
-        time.sleep(1)
+    # relay = relayop()
+    # time.sleep(1)
+    # relay.closeall()
+    # time.sleep(1)
+    # state = 0
+    #
+    #
+    # def opRelay(Relay):
+    #     global state
+    #     if state == 0:
+    #         relay.open(Relay)
+    #         state = 1
+    #     else:
+    #         relay.close(Relay)
+    #         state = 0
+    #
+    #
+    # for i in range(10):
+    #     opRelay(1)
+    #     time.sleep(1)
